b30c4b07d2645b20cd27ed9335bbcc6f83576e03
Megan Horsfield <meganthadev@gmail.com>
2020-07-21 05:17:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,16 +1,7 @@
-<<<<<<< HEAD
 function writeCards(names, event){
     let array = []
     for(let i=0; i<names.length; i++){
         array.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
     }
     return array;
-=======
-const cards = ["Ada", "Brendan", "Ali"], "birthday")] 
-
-function writeCards(cards) {
-  for (let i = 0; i < cards.length; i++) { console.log('Thank you, ${cards[i]}, for the wonderful birthday gift!');
-  }
-  return cards;
->>>>>>> 3dd836cc25a499d7223d9bbafdde1b522bceadbd
 }

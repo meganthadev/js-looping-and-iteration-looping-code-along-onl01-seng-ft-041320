c93a68556235204da158725e1fce299cafbd9db2
Megan Horsfield <meganthadev@gmail.com>
2020-07-21 05:07:36 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// Code your solutions in this file
+function writeCards(names, event){
+    let array = []
+    for(let i=0; i<names.length; i++){
+        array.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    }
+    return array;
+}
